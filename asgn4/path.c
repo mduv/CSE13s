4dbecb2d5f5ab728cfb144a1eef832086aa320a7
Mohan Duvvuri <mduvvuri@ucsc.edu>
2021-10-23 16:48:05 -0700

    plz

--- asgn4/path.c
+++ asgn4/path.c
@@ -68,7 +68,7 @@ uint32_t path_length(Path *p) {
 
 void path_copy(Path *dst, Path *src) {
     stack_copy(dst->vertices, src->vertices);
-    dst->length = src->length;
+    dst->length = path_length(src);
 }
 
 void path_print(Path *p, FILE *outfile, char *cities[]) {
