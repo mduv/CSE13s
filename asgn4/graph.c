fe82149dc108fb47c669c0a0b0eedb24a502d834
Mohan Duvvuri <mduvvuri@ucsc.edu>
2021-10-23 16:03:44 -0700

    fixed bug

--- asgn4/graph.c
+++ asgn4/graph.c
@@ -46,7 +46,7 @@ uint32_t graph_vertices(Graph *G) {
 }
 
 bool graph_add_edge(Graph *G, uint32_t i, uint32_t j, uint32_t k) {
-    if ((i > 0 && i < G->vertices) || (j > 0 && j < G->vertices)) {
+    if ((verticesWithinBounds(i) && verticesWithinBounds(j))) {
         G->matrix[i][j] = k;
         if (G->undirected) {
             G->matrix[j][i] = k;
