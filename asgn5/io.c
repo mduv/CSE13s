c124619e7a14c81f550feb187c9d2539c70e07ee
Mohan Duvvuri <mduvvuri@ucsc.edu>
2021-11-05 13:45:33 -0700

    read_bytes

--- asgn5/io.c
+++ asgn5/io.c
@@ -5,43 +5,31 @@
 #include <stdint.h>
 #include <unistd.h>
 
-// static uint8_t write_bit_top = 0;
-// static uint8_t write_bit_buffer[BLOCK] = {0};
 
-// int read_bytes(int infile, uint8_t *buf, int nbytes) {
-//     // create two variables
-//     // one for bytes read so far during this function,
-//     while((read(infile, buf + bytes_read_in, nbytes - bytes_read_in)) > 0) {
-//         bytes_read += ;
-//         if(bytes_read_in == nbytes) {
-//             break;
-//         }
-//     }
-//     return bytes_read;
-// }
+
 
 int read_bytes(int infile, uint8_t *buf, int nbytes) {
     int numBytesRead = 0;
-    while (nbytes > 0)
-    {
-        int n = read(infile, buf+numBytesRead, nbytes);
-
-        if (n == 0) {
-            break;
+    if (infile != 0) {
+        while (nbytes > 0) {
+            int n = read(infile, buf+numBytesRead, nbytes);
+            if (n <= 0) {
+                break;
+            }
+            numBytesRead += n;
+            nbytes -= n;
         }
-        numBytesRead += n;
-        nbytes -= n;
+        return numBytesRead;
     }
-    return numBytesRead;
-    
+    return 0;
 }
 
-int write_bytes(int outfile, uint8_t *buf, int nbytes);
+// int write_bytes(int outfile, uint8_t *buf, int nbytes);
 
-bool read_bit(int infile, uint8_t *bit);
+// bool read_bit(int infile, uint8_t *bit);
 
-void write_code(int outfile, Code *c);
+// void write_code(int outfile, Code *c);
 
-void flush_codes(int outfile);
+// void flush_codes(int outfile);
 
 
