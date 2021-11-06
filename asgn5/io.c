fbdb7b54b350b149ba381c0c19d3bb5954f7a4e6
Mohan Duvvuri <mduvvuri@ucsc.edu>
2021-11-06 14:16:07 -0700

    io

--- asgn5/io.c
+++ asgn5/io.c
@@ -6,8 +6,7 @@
 #include <unistd.h>
 #include <stdio.h>
 
-
-
+#define BITS_IN_BLOCK         (BLOCK * 8)             // 4KB blocks.
 
 
 
@@ -71,8 +70,56 @@ bool read_bit(int infile, uint8_t *bit) {
     return true;
 }
 
-// void write_code(int outfile, Code *c);
+/* The same bit-buffering logic used in read_bit() will be used in here as well. This function will also
+make use of a static buffer (we recommend this buffer to be static to the file, not just this function) and
+an index. Each bit in the code c will be buffered into the buffer. The bits will be buffered starting from the
+0th bit in c. When the buffer of BLOCK bytes is filled with bits, write the contents of the buffer to outfile. */
 
-// void flush_codes(int outfile);
+static uint8_t write_code_buffer[BLOCK] = {0};
+static uint32_t index = 0;
+
+
+void write_code(int outfile, Code *c) {
+    for (uint32_t i = 0; i < code_size(c); i++) {
+        code_get_bit(c, i); // code bit
+        // write code bit into correct index of buffer
+        uint32_t k = index / 8;  // index of which byte in buffer
+        uint32_t pos = index % 8;  // bit position in uint8 bit in buffer
+        uint8_t flag = 1 << pos;  // shifted position
+        if (code_get_bit(c, i) == 1) {
+            write_code_buffer[k] = code_get_bit(c, i) | flag;
+        } else {
+            flag = ~flag;
+            write_code_buffer[k] = code_get_bit(c, i) & flag;
+        }
+        index++;
+        if (index == BITS_IN_BLOCK) {
+            write_bytes(outfile, write_code_buffer, BLOCK);
+            index = 0;
+            // write_code_buffer[BLOCK] = {0};
+        }
+    }
+}
+
+bool clear_bit(uint32_t bit_position) {
+    if (bit_position < (BITS_IN_BLOCK)) {
+        uint32_t k = bit_position / 8;  // index of which byte in buffer
+        uint32_t pos = bit_position % 8;  // bit position in uint8 bit in buffer
+        uint8_t flag = 1 << pos;  // shifted position
+        flag = ~flag;
+        write_code_buffer[k] = write_code_buffer[k] & flag;
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void flush_codes(int outfile) {
+    for (uint32_t i = index; i < BITS_IN_BLOCK; i++) {
+        clear_bit(i);
+    }
+    write_bytes(outfile, write_code_buffer, BLOCK);
+}
+
 
 
