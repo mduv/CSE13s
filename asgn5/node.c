bc7b0205d26497f1c29136095a18104bdbb66be4
Mohan Duvvuri <mduvvuri@ucsc.edu>
2021-11-07 21:53:06 -0800

    valgrind fr

--- asgn5/node.c
+++ asgn5/node.c
@@ -6,21 +6,25 @@
 #include <unistd.h>
 
 // The constructor for a node. Sets the node’s symbol as symbol and its frequency as frequency.
-Node *node_create(uint8_t symbol, uint64_t frequency) {     
-    Node *n = (Node *) malloc(sizeof(Node));                
+Node *node_create(uint8_t symbol, uint64_t frequency) {
+    Node *n = (Node *) malloc(sizeof(Node));
     if (n) {
-        if (n->left == NULL && n->right == NULL) {     // make sure left and right pointers are NULL
-            n->symbol = symbol;                         // sets the node’s symbol as symbol
-            n->frequency = frequency;                   // sets the node’s frequency as frequency
-        }
+        // if (n->left == NULL && n->right == NULL) { // make sure left and right pointers are NULL
+        //     n->symbol = symbol; // sets the node’s symbol as symbol
+        //     n->frequency = frequency; // sets the node’s frequency as frequency
+        // }
+        n->left = NULL;
+        n->right = NULL; // make sure left and right pointers are NULL
+        n->symbol = symbol; // sets the node’s symbol as symbol
+        n->frequency = frequency; // sets the node’s frequency as frequency
     }
     return n;
 }
 
 // The destructor for a node.
 void node_delete(Node **n) {
-    free(*n);                                           // free memory for a node
-    *n = NULL;                                          // set pointer to NULL
+    free(*n); // free memory for a node
+    *n = NULL; // set pointer to NULL
     return;
 }
 
@@ -29,7 +33,7 @@ void node_delete(Node **n) {
     its frequency the sum of its left child’s frequency and its right child’s frequency. */
 Node *node_join(Node *left, Node *right) {
     uint64_t left_frequency = 0;
-    
+
     if (left) {
         left_frequency = left->frequency;
     }
@@ -37,12 +41,15 @@ Node *node_join(Node *left, Node *right) {
     if (right) {
         right_frequency = right->frequency;
     }
-    uint64_t parent_frequency = left_frequency + right_frequency;  // parent node's frequency is the sum of its left child's frequency and its right child's frequency
+    uint64_t parent_frequency
+        = left_frequency
+          + right_frequency; // parent node's frequency is the sum of its left child's frequency and its right child's frequency
     // uint64_t parent_frequency = (left->frequency) + (right->frequency);  // parent node's frequency is the sum of its left child's frequency and its right child's frequency
 
-    Node *parent_node = node_create('$', parent_frequency); // creates parent node with symbol '$' and frequency: parent_frequency
-    parent_node->left = left;                               // parent node's left child will be left
-    parent_node->right = right;                             // parent node's right child will be right
+    Node *parent_node = node_create('$',
+        parent_frequency); // creates parent node with symbol '$' and frequency: parent_frequency
+    parent_node->left = left; // parent node's left child will be left
+    parent_node->right = right; // parent node's right child will be right
     return parent_node;
 }
 
@@ -63,5 +70,3 @@ void node_print(Node *n) {
         printf("Freq: %" PRIu64 "\n", n->frequency);
     }
 }
-
-
