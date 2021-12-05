6729c3f08938b1b2b3505701131a710ec0644e50
Mohan Duvvuri <mduvvuri@ucsc.edu>
2021-12-05 03:41:30 -0800

    asdasdsiikl

--- asgn7/banhammer.c
+++ asgn7/banhammer.c
@@ -128,8 +128,8 @@ int main(int argc, char **argv) {
 
     /* Now that the lexicon of badspeak and oldspeak/newspeak translations has been populated, you
 c   an start to filter out words. Read words in from stdin using the supplied parsing module. */
-    //#define WORD "[a-zA-Z0-9_'-]+"
-    #define WORD "[bc]+"
+    #define WORD "[a-zA-Z0-9_'-]+"
+    //#define WORD "[bc]+"
     regex_t re;
     if (regcomp (&re , WORD , REG_EXTENDED)) {
         fprintf(stderr , "Failed to compile regex.\n");
