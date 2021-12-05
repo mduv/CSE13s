edfcc0452a2f36af44303034d3d04b75fe0b544e
Mohan Duvvuri <mduvvuri@ucsc.edu>
2021-12-05 05:24:55 -0800

    asdasdsiikln

--- asgn7/banhammer.c
+++ asgn7/banhammer.c
@@ -128,7 +128,7 @@ int main(int argc, char **argv) {
 
     /* Now that the lexicon of badspeak and oldspeak/newspeak translations has been populated, you
 c   an start to filter out words. Read words in from stdin using the supplied parsing module. */
-    #define WORD "[a-zA-Z0-9_'-]+"
+    #define WORD "(?i)[a-zA-Z0-9_'-]+"
     regex_t re;
     if (regcomp (&re , WORD , REG_EXTENDED)) {
         fprintf(stderr , "Failed to compile regex.\n");
