f8a22225bf837cc6594d2bac9b525112110aabc5
Mohan Duvvuri <mduvvuri@ucsc.edu>
2021-12-05 00:14:39 -0800

    banhammers

--- asgn7/banhammer.c
+++ asgn7/banhammer.c
@@ -134,7 +134,7 @@ int main(int argc, char **argv) {
 c   an start to filter out words. Read words in from stdin using the supplied parsing module. */
     #define WORD "[a-zA-Z0-9/-_']+"
     regex_t re;
-    if (regcomp (&re , WORD , REG_EXTENDED)) {
+    if (regcomp (&re , WORD , REG_BASIC)) {
         fprintf(stderr , "Failed to compile regex.\n");
     }
 
