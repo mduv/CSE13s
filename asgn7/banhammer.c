762153802d8c504f4997c72acf8d6c235150c42d
Mohan Duvvuri <mduvvuri@ucsc.edu>
2021-12-05 22:16:23 -0800

    af2

--- asgn7/banhammer.c
+++ asgn7/banhammer.c
@@ -130,14 +130,6 @@ c   an start to filter out words. Read words in from stdin using the supplied pa
     bool thoughtcrime = false;
     bool rightspeak = false;
 
-    // HashTable *badspeak_ht = ht_create(bf_count(bf));
-    // HashTable *oldspeak_ht = ht_create(bf_count(bf));
-
-    // Node *badspeak_nodes[100000];
-    // Node *oldspeak_nodes[100000];
-    // int bsn = 0;
-    // int osn = 0;
-
     Node* badspeak_bst = bst_create();
     Node* oldspeak_bst = bst_create();
 
@@ -149,15 +141,11 @@ c   an start to filter out words. Read words in from stdin using the supplied pa
             if (check != NULL) {
                 // thoughtcrime
                 if (check->newspeak == NULL) {
-                    //ht_insert(badspeak_ht, word, NULL);
                     thoughtcrime = true;
-                    //badspeak_nodes[bsn++] = node_create(word, NULL);
                     badspeak_bst = bst_insert(badspeak_bst, word, NULL);
                 } else {
                     // Rightspeak
-                    //ht_insert(oldspeak_ht, word, check->newspeak);
                     rightspeak = true;
-                    //oldspeak_nodes[osn++] = node_create(word, check->newspeak);
                     oldspeak_bst = bst_insert(oldspeak_bst, word, check->newspeak);
                 }
             }
@@ -184,16 +172,8 @@ c   an start to filter out words. Read words in from stdin using the supplied pa
         free(ns_contents);
         bf_delete(&bf);
         ht_delete(&ht);
-        // ht_delete(&badspeak_ht);
-        // ht_delete(&oldspeak_ht);
         clear_words();
         regfree(&re);
-        // for (int i = 0; i < bsn; i++) {
-        //     node_delete(&badspeak_nodes[i]);
-        // }
-        // for (int i = 0; i < osn; i++) {
-        //     node_delete(&oldspeak_nodes[i]);
-        // }
         bst_delete(&badspeak_bst);
         bst_delete(&oldspeak_bst);
         
@@ -202,32 +182,16 @@ c   an start to filter out words. Read words in from stdin using the supplied pa
 
     if (thoughtcrime && !rightspeak) {
         printf("%s", badspeak_message);
-        //ht_print(badspeak_ht);
-        // for (int i = 0; i < bsn; i++) {
-        //     node_print(badspeak_nodes[i]);
-        // }
         bst_print(badspeak_bst);
     }
 
     if (!thoughtcrime && rightspeak) {
         printf("%s", goodspeak_message);
-        //ht_print(oldspeak_ht);
-        // for (int i = 0; i < osn; i++) {
-        //     node_print(oldspeak_nodes[i]);
-        // }
         bst_print(oldspeak_bst);
     }
 
     if (thoughtcrime && rightspeak) {
         printf("%s", mixspeak_message);
-        //ht_print(badspeak_ht);
-        //ht_print(oldspeak_ht);
-        // for (int i = 0; i < bsn; i++) {
-        //     node_print(badspeak_nodes[i]);
-        // }
-        // for (int i = 0; i < osn; i++) {
-        //     node_print(oldspeak_nodes[i]);
-        // }
         bst_print(badspeak_bst);
         bst_print(oldspeak_bst);
     }
@@ -242,14 +206,6 @@ c   an start to filter out words. Read words in from stdin using the supplied pa
     free(ns_contents);
     bf_delete(&bf);
     ht_delete(&ht);
-    // ht_delete(&badspeak_ht);
-    // ht_delete(&oldspeak_ht);
-    // for (int i = 0; i < bsn; i++) {
-    //     node_delete(&badspeak_nodes[i]);
-    // }
-    // for (int i = 0; i < osn; i++) {
-    //     node_delete(&oldspeak_nodes[i]);
-    // } 
     bst_delete(&badspeak_bst);
     bst_delete(&oldspeak_bst);
 
